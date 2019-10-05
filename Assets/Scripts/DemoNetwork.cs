a04cb52deb562f6e599714acd59850ba84cbda07
Moosan <washer2016@outlook.jp>
2019-10-05 16:50:41 +0900

    LoomTest

---
+++ Assets/Scripts/DemoNetwork.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Network = Photon.Pun.PhotonNetwork;
+public class DemoNetwork : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    public void Connect()
+    {
+        Network.ConnectUsingSettings();
+        Debug.Log("a");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Network.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        var v = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        Network.Instantiate("GamePlayer",v,Quaternion.identity);
+    }
+    
+    public void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            if (Network.IsConnected) return;
+            Connect();
+        }
+    }
+}
+
